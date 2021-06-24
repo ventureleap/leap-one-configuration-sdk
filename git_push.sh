4ce9a5e7c38dd29a2d144a47d9dae639dc24ed1f
GitHub Action <action@github.com>
2021-06-24 15:07:29 +0000

    Configuration Service commit hash: 4499b2f

--- git_push.sh
+++ git_push.sh
@@ -18,7 +18,7 @@ if [ "$git_repo_id" = "" ]; then
 fi
 
 if [ "$release_note" = "" ]; then
-    release_note="Configuration Service commit hash: 57e21a9"
+    release_note="Configuration Service commit hash: 4499b2f"
     echo "[INFO] No command line input provided. Set \$release_note to $release_note"
 fi
 
